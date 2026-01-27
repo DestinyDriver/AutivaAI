4a0dd91920bfd3aa62e7803969b8225f81242a02
DestinyDriver <destinydriverx@gmail.com>
2026-01-27 23:02:06 +0530

    Modfiy: Sidebar Header Icon

--- components/team-switcher.jsx
+++ components/team-switcher.jsx
@@ -1,7 +1,14 @@
 "use client";
 
 import * as React from "react";
-import { ChevronsUpDown, Github, Link, Plus, Star } from "lucide-react";
+import {
+  BrainCircuit,
+  ChevronsUpDown,
+  Github,
+  Link,
+  Plus,
+  Star,
+} from "lucide-react";
 
 import {
   DropdownMenu,
@@ -37,7 +44,7 @@ export function TeamSwitcher({ teams }) {
               className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
             >
               <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-8 items-center justify-center rounded-lg">
-                <activeTeam.logo className="size-4" />
+                <BrainCircuit className="size-4" />
               </div>
               <div className="grid flex-1 text-left text-sm leading-tight">
                 <span className="truncate font-medium">AutivaAI</span>
